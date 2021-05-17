1ed0e8044d280d84aaf7776afeb306aa32b40b2b
Adi-Marom <adimaro@bgu.ac.il>
2021-05-17 19:59:07 +0300

    ...

--- src/View/MyViewController.java
+++ src/View/MyViewController.java
@@ -3,22 +3,32 @@ package View;
 import algorithms.mazeGenerators.Maze;
 import algorithms.mazeGenerators.MyMazeGenerator;
 import algorithms.search.SearchableMaze;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+
+import java.awt.*;
+import java.io.File;
+
+import java.io.IOException;
+import java.nio.file.Files;
+
 
 public class MyViewController implements IView{
     public MyMazeGenerator generator;
     public TextField textField_mazeRows;
     public TextField textField_mazeColumns;
     public MazeDisplayer mazeDisplayer;
+    public  Maze maze;
 
     public void generateMaze(ActionEvent actionEvent) {
         if(generator == null)
             generator = new MyMazeGenerator();
         int rows = Integer.parseInt(textField_mazeRows.getText());
         int cols = Integer.parseInt(textField_mazeColumns.getText());
-        Maze maze = generator.generate(rows, cols);
+        maze = generator.generate(rows, cols);
         mazeDisplayer.drawMaze(maze);
     }
 
@@ -26,5 +36,41 @@ public class MyViewController implements IView{
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setContentText("Solving maze...");
         alert.show();
+    }
+    public  void CloseB(){
+        System.out.println("closessssssss ");
+        //TODO smart exit from project
+        Platform.exit();
+    }
+    public void SaveB() throws IOException {
+        if(maze != null){
+           byte[] bmaze= maze.toByteArray();
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Save Image");
+            File file = fileChooser.showSaveDialog(mazeDisplayer.getScene().getWindow());
+            if (file != null) {
+                Files.write(file.toPath(), bmaze);
+
+                System.out.println("shit");
+            }
+        }
+    }
+    public void LoadB(){ //TODO implent this function
+       /* FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Resource File");
+        fileChooser.showOpenDialog(mazeDisplayer.getScene().getWindow());
+        File file = fileChooser.showOpenDialog(mazeDisplayer.getScene().getWindow());
+        System.out.println("staart");
+        if (file != null) {
+            try {
+                desktop.open(file);
+            } catch (IOException ex) {
+
+            }
+            System.out.println("staart");
+
+
+        }
+*/
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,5 +1,6 @@
 package sample;
 
+import View.FirstController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -9,12 +10,27 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../View/MyView.fxml"));
+    /*public void start(Stage primaryStage) throws Exception{
+        //Parent root = FXMLLoader.load(getClass().getResource("../View/MyView.fxml"));
+        Parent root1 = FXMLLoader.load(getClass().getResource("C:\\Users\\adidi\\IdeaProjects\\ATP-PartCa\\src\\View\\First.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        //primaryStage.setScene(new Scene(root, 800, 500));
+        primaryStage.setScene(new Scene(root1, 900, 500));
+
+        primaryStage.show();
+    }*/
+    public void start(Stage primaryStage) throws Exception{
+
+        FXMLLoader loader = new FXMLLoader();
+        Parent root = loader.load(getClass().getResource("../View/First.fxml").openStream());
+        primaryStage.setTitle("MAZES & DRAGONS");
+        Scene scene = new Scene(root, 590, 402);
+        scene.getStylesheets().add(getClass().getResource("/View/ViewStyle.css").toExternalForm());
+        primaryStage.setScene(scene);
+        //FirstController.currentStage = primaryStage;
         primaryStage.show();
     }
+
 
     public static void main(String[] args) {
         launch(args);
